9049557a265252b0537c7bb56dc354746a35b0ae
sudarshanmku <sdsmart01@gmail.com>
2014-11-22 17:32:32 +0530

    run_analysis.R submitted

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,10 @@
+##You should create one R script called run_analysis.R that does the following. 
+   #1.Merges the training and the test sets to create one data set.
+   #2.Extracts only the measurements on the mean and standard deviation for each measurement. 
+   #3.Uses descriptive activity names to name the activities in the data set
+   #4.Appropriately labels the data set with descriptive variable names. 
+   #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 setwd("~/Documents/git/Getting_Cleaning_Data")
 training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
 training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
@@ -43,4 +50,4 @@ tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$s
 # Remove the subject and activity column, since a mean of those has no use
 tidy[,90] = NULL
 tidy[,89] = NULL
-write.table(tidy, "tidy.txt", sep="\t")
+write.table(tidy, "tidy.txt", sep="\t",row.name=FALSE)
